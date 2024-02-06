a96452812aab169a3baabfdd764f55273b973de0
JamilServices <jamilservicos@gmail.com>
2024-02-06 20:28:09 -0300

    feat: add clean-build script to remove build and pack files

---
+++ scripts/clean-build.cjs
@@ -0,0 +1,32 @@
+"use strict";
+const {resolve} = require("node:path");
+const fs = require('node:fs');
+const fastGlob = require('fast-glob');
+const {globSync} = fastGlob;
+
+const rootPath = resolve(__dirname, "..");
+const tmps = [
+    `${rootPath}/build`
+];
+
+const removeItem = (item) => {
+    try {
+        fs.rm(item, { recursive: true, force: true }, err => {
+            if (err) throw err;
+            console.log(`${item} is deleted!`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const packfiles = globSync(`${rootPath}/*.tgz`);
+
+if(packfiles.length >= 1) {
+    packfiles.forEach(item => {
+        removeItem(item);
+    });
+}
+tmps.forEach(item => {
+    removeItem(item);
+});
