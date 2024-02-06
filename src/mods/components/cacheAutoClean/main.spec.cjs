031ad40a337a3036c66b48e26b29f4607787124b
JamilServices <jamilservicos@gmail.com>
2024-02-06 20:18:19 -0300

    feat(cacheAutoClean): add mock addItem test
    Added a test to mock the `addItem` function in the `cacheAutoClean` module. The test simulates adding an item to the cache and verifies that the item is correctly stored.

--- src/mods/components/cacheAutoClean/main.spec.cjs
+++ src/mods/components/cacheAutoClean/main.spec.cjs
@@ -1,10 +1,40 @@
 "use strict";
 
-const {describe} = require("node:test");
+const {describe, it} = require("node:test");
 const {cacheAutoClean} = require("./main.cjs");
+const {ok} = require("node:assert");
+
+const items = new Map();
 
 exports = module.exports = (mock) => {
-    describe("mods/components/cacheAutoClean/main.js tests", () => {
-        console.log("mods/components/cacheAutoClean", cacheAutoClean);
+    const {data, itemId, itemJson} = mock;
+    const cache = {
+        get list () {
+            return Array.from(items.keys());
+        },
+        store: () => {
+            return {
+                items,
+                settings: { ttl: 120000, min: 60000, clean: 15000 }
+            }
+        }
+    };
+    const test = {
+        next: 1,
+        limit: 5,
+        interval: 500
+    };
+    describe("component cacheAutoClean module tests", () => {
+        it("mock addItem", () => {
+            const itemTimestamp = Date.now();
+            cacheAutoClean({cache, test});
+            items.set(itemId, {
+                key: itemId,
+                url: data.url,
+                ttl: data.ttl,
+                timestamp: itemTimestamp,
+                json: itemJson
+            });
+        });
     });
 };
