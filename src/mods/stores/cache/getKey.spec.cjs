8a0b156ae8b38a729d3996254e800dbf53658c81
JamilServices <jamilservicos@gmail.com>
2024-02-06 13:22:30 -0300

    refactor getKey.spec.cjs to use itemId from mock

--- src/mods/stores/cache/getKey.spec.cjs
+++ src/mods/stores/cache/getKey.spec.cjs
@@ -4,21 +4,20 @@ const {describe, it} = require("node:test");
 const {getKey, getUUID} = require("./getKey.cjs");
 const {ok, equal} = require("node:assert");
 
-const key = 'itemId-iDufQhdISvayx5VNIHQW7KX9m7TiYqlx6r1eIDQ6qaA';
 const regex = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
 
 exports = module.exports = (mock) => {
-    const {data} = mock;
+    const {data, itemId} = mock;
 
     describe("CacheStoreInterface getKey unit tests", () => {
         it("test getKey", () => {
             const result = getKey(data.url);
             ok(typeof result === "string");
             ok(result.startsWith("itemId"));
-            equal(result, key);
+            equal(result, itemId);
         });
         it("test getUUID", () => {
-            const result = getUUID(data.url);
+            const result = getUUID();
             const uuid = result.replace('instanceId-', '');
             ok(typeof result === "string");
             ok(result.startsWith("instanceId"));
