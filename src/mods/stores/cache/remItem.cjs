3f308162b37f34827ad677c253a174ee674d7f46
JamilServices <jamilservicos@gmail.com>
2024-02-07 02:18:48 -0300

    feat(stores): add remItem function to remove cache item
    Adds the remItem function to the cache store module. This function allows for the removal of a specific cache item based on its URL. The function generates a unique key from the URL and deletes the corresponding item from the cache. It verifies that the operation is performed on an instance of the CacheStoreInterface and that the URL meets the minimum length requirement. The function also ensures the successful removal of the item by checking the absence of the key in the cache after the deletion attempt.
    
    The remItem function is frozen to prevent modifications, ensuring the integrity and reliability of the cache item removal mechanism. It is exported as part of the module's public interface, making it available for cache management operations in other parts of the application.

--- src/mods/stores/cache/remItem.cjs
+++ src/mods/stores/cache/remItem.cjs
@@ -1,5 +1,18 @@
 "use strict";
-
+/**
+ * Removes a specific item from the cache based on its URL.
+ * This function aims to delete a cache item by generating a unique key from the provided URL
+ * and removing the corresponding item from the cache. It verifies that the operation is performed
+ * on an instance of the CacheStoreInterface and that the URL meets the minimum length requirement.
+ * The function ensures that the item is successfully removed by checking the absence of the key
+ * in the cache after the deletion attempt.
+ *
+ * @function remItem
+ * @param {Object} data - The data required to remove the cache item.
+ * @param {CacheStoreInterface} data.instance - The cache store instance from which to remove the item.
+ * @param {string} data.url - The URL associated with the cache item to be removed.
+ * @returns {boolean} - Returns true if the item is successfully removed; otherwise, false.
+ */
 const remItem = (data) => {
     const {instance, url} = data;
     try {
@@ -15,7 +28,16 @@ const remItem = (data) => {
     }
     return false;
 };
+/**
+ * Freezes the `remItem` function to prevent any modifications, ensuring the integrity
+ * and reliability of the cache item removal mechanism. This action reinforces the principle
+ * of immutability for critical functionalities within the application.
+ */
 Object.freeze(remItem);
-exports = module.exports = {
-    remItem
-};
+/**
+ * Exports the `remItem` function, making it available for cache management operations
+ * within other parts of the application. By providing this function as part of the module's
+ * public interface, other components can easily manage cached data, contributing to a more
+ * efficient and clean application architecture.
+ */
+exports = module.exports = {remItem};
