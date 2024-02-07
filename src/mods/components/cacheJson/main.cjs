1dfe18615c857c22af2fea4b628ef1bbf68d487f
JamilServices <jamilservicos@gmail.com>
2024-02-06 21:57:27 -0300

    feat(cacheJson): add function to prepare save schema
    This commit adds a new function `prepareSave` to the `cacheJson` module in order to prepare the save schema based on the data received. The function takes into account the settings, request, and size of the data to determine the appropriate values for the TTL and URL in the schema. This allows for more flexibility in handling dynamic requests and caching large JSON responses.

--- src/mods/components/cacheJson/main.cjs
+++ src/mods/components/cacheJson/main.cjs
@@ -13,7 +13,12 @@ const fetchJson = async (url) => {
         });
         if(requestJson?.ok) {
             const text = await requestJson.text();
-            if(text) return text;
+            if(text) {
+                return {
+                    json: text,
+                    size: new Blob([text]).size
+                };
+            }
             return undefined;
         }
     } catch (err) {
@@ -33,22 +38,35 @@ const saveJson = (schema) => {
     } catch {}
     return undefined;
 };
+const prepareSave = (data) => {
+    const {settings, req, size} = data;
+    const schema = {
+        ttl: settings.ttl,
+        url: req.url
+    };
+
+    if(parseInt(req.ttl) >= settings.min) schema.ttl = parseInt(req.ttl);
+    if((req.dynamic && size)
+        && (size >= settings.min) && (schema.ttl >= size)) {
+        schema.ttl = parseInt(size);
+    }
+    return schema;
+};
 const cacheJson = async (data) => {
     try {
         const {store, settings, req} = data;
         if(typeof req.url === "undefined") return undefined;
         const fromCache = store.getItem({url: req.url});
         if(fromCache) return {...fromCache, cache: true};
-        const schema = {
-            ttl: settings.ttl,
-            url: req.url
-        };
-        if(parseInt(req.ttl) >= settings.min) schema.ttl = parseInt(req.ttl);
         const fromFetch = await fetchJson(req.url);
-        if(fromFetch) {
-            const obj = safeDestr(fromFetch);
+        if(fromFetch?.json) {
+            const obj = safeDestr(fromFetch.json);
             if(obj) {
                 const tosave = JSON.stringify(obj);
+                const schema = prepareSave({
+                    settings, req,
+                    size: fromFetch.size
+                });
                 return saveJson({
                     ...schema,
                     json: tosave,
