d686947a6953c47455e237b61cb858d9ef1c0a44
JamilServices <jamilservicos@gmail.com>
2024-02-07 02:20:19 -0300

    feat(cache): add addItem function
    - Add the `addItem` function to the cache store.
    - The function validates the input data for caching operations.
    - If the data passes validation checks, the function adds a new item to the cache.

--- src/mods/stores/cache/addItem.cjs
+++ src/mods/stores/cache/addItem.cjs
@@ -1,5 +1,17 @@
 "use strict";
-
+/**
+ * Validates the input data for caching operations.
+ * This function checks if the provided data meets the necessary criteria for adding an item to the cache,
+ * including type checks for each piece of data and ensuring the instance conforms to the CacheStoreInterface.
+ *
+ * @function validate
+ * @param {Object} data - The data to be validated.
+ * @param {Object} data.instance - The cache store instance where the item will be added.
+ * @param {number} data.ttl - The time-to-live (TTL) for the cache item.
+ * @param {string} data.url - The URL associated with the cache item.
+ * @param {string} data.json - The JSON string to be cached.
+ * @returns {boolean} Returns true if the data passes all validation checks; otherwise, false.
+ */
 const validate = (data) => {
     try {
         const {instance, ttl, url, json} = data;
@@ -16,7 +28,19 @@ const validate = (data) => {
     }
     return false;
 };
-
+/**
+ * Adds a new item to the cache, if it passes validation checks.
+ * This function attempts to add a new item to the cache, identified by a URL, with a specified TTL and content.
+ * It first validates the input data and, if successful, stores the item in the cache using a unique key.
+ *
+ * @function addItem
+ * @param {Object} data - The data for the cache item to be added.
+ * @param {Object} data.instance - The cache store instance where the item will be added.
+ * @param {number} data.ttl - The time-to-live (TTL) for the cache item.
+ * @param {string} data.url - The URL associated with the cache item.
+ * @param {string} data.json - The JSON string to be cached.
+ * @returns {Object|undefined} Returns the cached item object if addition is successful; otherwise, undefined.
+ */
 const addItem = (data) => {
     try {
         const {instance, ttl, url, json} = data;
@@ -38,7 +62,12 @@ const addItem = (data) => {
     }
     return undefined;
 };
+/**
+ * Freezes the `addItem` function to prevent modifications, ensuring its integrity and reliability.
+ */
 Object.freeze(addItem);
-exports = module.exports = {
-    addItem
-};
+/**
+ * Exports the `addItem` function, making it available for cache management operations within other parts of the application.
+ * This function is essential for maintaining an up-to-date and efficient cache system by adding new items as needed.
+ */
+exports = module.exports = {addItem};
